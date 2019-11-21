decb92e6c084af7466cfc918bcba187bf266fa00
Karishma Saini <ksaini7595@gmail.com>
2019-11-20 23:40:08 -0500

    Adding robofriends in Github

---
+++ build/precache-manifest.8941226bb8e88efbb60b3e4773316dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c776c619234af6362f8ba49730f947ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "93ad89711571bfb1e16a",
+    "url": "/static/css/main.5830382f.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "93ad89711571bfb1e16a",
+    "url": "/static/js/main.5ff5eb3e.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  }
+]);
